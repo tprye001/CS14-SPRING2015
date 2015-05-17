a7f250972d1db95b1c0d06146dcbd6389d317590
tprye001 <tprye001@ucr.edu>
2015-05-17 19:32:42 +0000

    lab6

--- lab06/main.cc
+++ lab06/main.cc
@@ -2,57 +2,137 @@
 #include "selectionsort.h"
 #include <vector>
 #include <map>
+#include <list>
+#include <unordered_map>
 #include <utility>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    // //VECTOR
-    // vector<int> v;
-    // v.push_back(2);
-    // v.push_back(4);
-    // v.push_back(5);
-    // v.push_back(1);
-    // v.push_back(8);
-    // v.push_back(9);
-    // cout << "pre:  ";
-    // for(int i = 0; i < v.size(); i++)
-    // {
-    //     cout << v.at(i) << "  ";
-    // }
-    // cout << endl;
+    //VECTOR
+    vector<int> v;
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(1);
+    v.push_back(8);
+    v.push_back(9);
+    cout << "pre:  ";
+    for(int i = 0; i < v.size(); i++)
+    {
+        cout << v.at(i) << "  ";
+    }
+    cout << endl;
 
-    // selectionsort(v);
-    // cout << "post: ";
-    // for(int i = 0; i < v.size(); i++)
-    // {
-    //     cout << v.at(i) << "  ";
-    // }
-    // cout << endl;
+    selectionsort(v);
+    cout << "post: ";
+    for(int i = 0; i < v.size(); i++)
+    {
+        cout << v.at(i) << "  ";
+    }
+    cout << endl;
     
-    map<int, int> m;
-    m.insert( pair<int, int> (1,2));
-    m.insert( pair<int, int> (3,-1));
-    m.insert( pair<int, int> (-1,3));
-    m.insert( pair<int, int> (0,0));
-    m.insert( pair<int, int> (2,3));
-    m.insert( pair<int, int> (1,2));
-    m.insert( pair<int, int> (1,-2));
-    m.insert( pair<int, int> (8,10));    
+    list<pair<int, int> > m;
+    
+    m.push_back( pair<int, int> (1,2));
+    m.push_back( pair<int, int> (3,-1));
+    m.push_back( pair<int, int> (-1,3));
+    m.push_back( pair<int, int> (0,0));
+    m.push_back( pair<int, int> (2,3));
+    m.push_back( pair<int, int> (1,2));
+    m.push_back( pair<int, int> (1,-2));
+    m.push_back( pair<int, int> (8,10));    
     
     cout << "pre:  ";
-    for(typename map<int, int>::iterator it = m.begin(); it != m.end(); it++)
+    for(typename list<pair<int, int> >::iterator it = m.begin(); 
+        it != m.end(); it++)
     {
         cout << "(" << it->first << "," << it->second << ") ";
     }
     cout << endl;
     selectionsort(m);
     cout << "post: ";
-    for(typename map<int, int>::iterator it = m.begin(); it != m.end(); it++)
+    for(typename list<pair<int, int> >::iterator it = m.begin(); 
+        it != m.end(); it++)
     {
         cout << "(" << it->first << "," << it->second << ") ";
     }
     cout << endl;
+    cout << "0 copies and " << 3*moves << " moves" << endl;
+    
+    list<pair<int, int> > n;
+    n.push_back( pair<int, int> (10,2));
+    n.push_back( pair<int, int> (-3,-1));
+    n.push_back( pair<int, int> (-8,0));
+    n.push_back( pair<int, int> (1,1));
+    n.push_back( pair<int, int> (1,1));
+    n.push_back( pair<int, int> (0,0));
+    n.push_back( pair<int, int> (10,2));
+    n.push_back( pair<int, int> (5,5));
+    n.push_back( pair<int, int> (5,-5)); 
+    n.push_back( pair<int, int> (0,0));
+    n.push_back( pair<int, int> (10,2));
+    
+    cout << "pre:  ";
+    for(typename list<pair<int, int> >::iterator it = n.begin(); 
+        it != n.end(); it++)
+    {
+        cout << "(" << it->first << "," << it->second << ") ";
+    }
+    cout << endl;
+    selectionsort(n);
+    cout << "post: ";
+    for(typename list<pair<int, int> >::iterator it = n.begin(); 
+        it != n.end(); it++)
+    {
+        cout << "(" << it->first << "," << it->second << ") ";
+    }
+    cout << endl;
+    cout << "0 copies and " << 3*moves << " moves" << endl;
+
+    cout << "pre:  ";
+    for(typename list<pair<int, int> >::iterator it = m.begin(); 
+        it != m.end(); it++)
+    {
+        cout << "(" << it->first << "," << it->second << ") ";
+    }
+    cout << endl;
+    selectionsort(m);
+    cout << "post: ";
+    for(typename list<pair<int, int> >::iterator it = m.begin(); 
+        it != m.end(); it++)
+    {
+        cout << "(" << it->first << "," << it->second << ") ";
+    }
+    cout << endl;
+    cout << "0 copies and " << 3*moves << " moves" << endl << endl; 
+    
+    cout << "Question 2. Part b" << endl;
+    list<pair<int, string> > o;
+    o.push_back( pair<int, string> (10, "start"));
+    o.push_back( pair<int, string> (10, "end"));
+    o.push_back( pair<int, string> (2, "2"));
+    o.push_back( pair<int, string> (1, "1"));
+    
+    cout << "pre: ";
+    for(typename list<pair<int, string> >::iterator it = o.begin(); 
+        it != o.end(); it++)
+    {
+        cout << "(" << it->first << "," << it->second << ") ";
+    }
+    cout << endl;
+    selectionsort(o);
+    cout << "post: ";
+    for(typename list<pair<int, string> >::iterator it = o.begin(); 
+        it != o.end(); it++)
+    {
+        cout << "(" << it->first << "," << it->second << ") ";
+    }
+    cout << endl;
+    
+    
+    
     return 0;
 }
--- lab06/selectionsort.h
+++ lab06/selectionsort.h
@@ -1,3 +1,10 @@
+/*
+// Name: Tricia Pryer
+// SID: 861051465
+// Approach: Used nested for loops to iterate through the list and 
+             swapped when necessary
+// 
+*/
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -5,23 +12,29 @@
 
 using namespace std;
 
+unsigned moves = 0;
 
 template< typename L >
 void selectionsort(L &l)
 {
+    moves = 0;
     typename L::iterator it = l.begin();
-    int s = l.size();
     
     for(; it != l.end(); it++)
     {
+        typename L::iterator min = it;
         for(typename L::iterator it2 = it; it2 != l.end(); it2++)
         {
             //if it2->value < it->value, then swap
-            if(it2 < it)
+            if(*it2 < *min)
             {
-                //swap(it2->second, it->second);
-                iter_swap(it2, it);
+                min = it2;
             }
+        }
+        if(it != min)
+        {
+            swap(*min, *it);
+            moves++;
         }
     }
 }
